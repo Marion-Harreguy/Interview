f102ebbef008143164eeb00cffbdbeda22ef8a4d
Marion-Harreguy <marionharreguy@gmail.com>
2020-04-09 16:05:14 +0200

    [func] adding CategoryController + methods BREAD + CategoryType

---
+++ api/src/Controller/CategoryController.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Category;
+use App\Repository\CategoryRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+/**
+ * @Route("/api/users/{id}/categories", name="category_")
+ */
+class InterviewController extends AbstractController
+{
+    /**
+     * TODO : définition des routes - annotations 
+     * TODO : écriture du BREAD
+     * TODO : methods des routes
+     * TODO : requirements des routes 
+     */
+
+    /**
+     * Liste toutes les categories d'un utilisateur
+     * 
+     * @Route("/", name="browse", methods={"GET"})
+     */
+    public function browse(CategoryRepository $categoryRepository)
+    {
+        return $this->json([
+            'message' => 'Welcome on the Browse method',
+            'path' => 'src/Controller/InterviewController.php',
+        ]);
+    }
+
+    /**
+     * Modifie / met à jour une catégorie d'un utilisateur
+     * 
+     * @Route("/{id}", name="edit", methods={"PUT", "PATCH"}, requirements={"id":"\d+"})
+     */
+    public function edit(Category $category, Request $request)
+    {
+        $form = $this->createForm(CategoryType::class, $user);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            // $user->setUpdatedAt(new \DateTime());
+
+            $this->getDoctrine()->getManager()->flush();
+
+            // return $this->redirectToRoute('');
+        }
+
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/UserController.php',
+        ]);
+    }
+
+    /**
+     * Créer une nouvelle catégorie pour l'utilisateur
+     * 
+     * @Route("/", name="add", methods={"POST"})
+     */
+    public function add(Request $request)
+    {
+        $category = new Category();
+        $form = $this->createForm(CategoryType::class, $user);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+
+            $em->persist($user);
+            $em->flush();
+
+            // return $this->redirectToRoute('');
+        }
+
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/UserController.php',
+        ]);
+    }
+
+    /**
+     * Supprime une catégorie de l'utilisateur
+     * 
+     * @Route("/{id}", name="delete", methods={"DELETE"}, requirements={"id":"\d+"})
+     */
+    public function delete(Category $category)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $em->remove($category);
+        $em->flush();
+
+        return $this->redirectToRoute('');
+    }
+}
--- api/var/cache/dev/url_matching_routes.php
+++ api/var/cache/dev/url_matching_routes.php
@@ -13,10 +13,11 @@ return [
         '/_profiler/search_bar' => [[['_route' => '_profiler_search_bar', '_controller' => 'web_profiler.controller.profiler::searchBarAction'], null, null, null, false, false, null]],
         '/_profiler/phpinfo' => [[['_route' => '_profiler_phpinfo', '_controller' => 'web_profiler.controller.profiler::phpinfoAction'], null, null, null, false, false, null]],
         '/_profiler/open' => [[['_route' => '_profiler_open_file', '_controller' => 'web_profiler.controller.profiler::openAction'], null, null, null, false, false, null]],
-        '/interviews' => [
+        '/api/interviews' => [
             [['_route' => 'interview_browse', '_controller' => 'App\\Controller\\InterviewController::browse'], null, ['GET' => 0], null, true, false, null],
             [['_route' => 'interview_add', '_controller' => 'App\\Controller\\InterviewController::add'], null, ['POST' => 0], null, true, false, null],
         ],
+        '/api/users' => [[['_route' => 'useruser_add', '_controller' => 'App\\Controller\\UserController::add'], null, ['POST' => 0], null, true, false, null]],
     ],
     [ // $regexpList
         0 => '{^(?'
@@ -35,8 +36,13 @@ return [
                         .'|(*:159)'
                     .')'
                 .')'
-                .'|/interviews/(\\d+)(?'
-                    .'|(*:189)'
+                .'|/api/(?'
+                    .'|interviews/(\\d+)(?'
+                        .'|(*:196)'
+                    .')'
+                    .'|users/(\\d+)(?'
+                        .'|(*:219)'
+                    .')'
                 .')'
             .')/?$}sDu',
     ],
@@ -48,10 +54,14 @@ return [
         136 => [[['_route' => '_profiler_exception', '_controller' => 'web_profiler.controller.exception_panel::body'], ['token'], null, null, false, false, null]],
         149 => [[['_route' => '_profiler_exception_css', '_controller' => 'web_profiler.controller.exception_panel::stylesheet'], ['token'], null, null, false, false, null]],
         159 => [[['_route' => '_profiler', '_controller' => 'web_profiler.controller.profiler::panelAction'], ['token'], null, null, false, true, null]],
-        189 => [
+        196 => [
             [['_route' => 'interview_read', '_controller' => 'App\\Controller\\InterviewController::read'], ['id'], ['GET' => 0], null, false, true, null],
             [['_route' => 'interview_edit', '_controller' => 'App\\Controller\\InterviewController::edit'], ['id'], ['PUT' => 0, 'PATCH' => 1], null, false, true, null],
             [['_route' => 'interview_delete', '_controller' => 'App\\Controller\\InterviewController::delete'], ['id'], ['DELETE' => 0], null, false, true, null],
+        ],
+        219 => [
+            [['_route' => 'useruser_read', '_controller' => 'App\\Controller\\UserController::read'], ['id'], ['GET' => 0], null, false, true, null],
+            [['_route' => 'useredit', '_controller' => 'App\\Controller\\UserController::edit'], ['id'], ['PUT' => 0, 'PATCH' => 1], null, false, true, null],
             [null, null, null, null, false, false, 0],
         ],
     ],
