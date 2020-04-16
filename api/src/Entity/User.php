6bfc9f61eb00c9fb6c8f6b41d5eb8b7d2dbc82c0
Marion-Harreguy <marionharreguy@gmail.com>
2020-04-16 10:39:43 +0200

    [edit] Validation constraints UserType

--- api/src/Entity/User.php
+++ api/src/Entity/User.php
@@ -5,6 +5,7 @@ namespace App\Entity;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Serializer\Annotation\Groups;
 use Symfony\Component\Validator\Constraints as Assert;
@@ -12,6 +13,7 @@ use Symfony\Component\Validator\Constraints as Assert;
 /**
  * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
  * @ORM\Table(name="`user`")
+ * @UniqueEntity(fields="email", message="Email already taken")
  */
 class User implements UserInterface
 {
@@ -38,7 +40,7 @@ class User implements UserInterface
 
     /**
      * @var string user email to authentificate
-     * @ORM\Column(type="string", length=200)
+     * @ORM\Column(type="string", length=200, unique=true)
      * @Assert\Email
      */
     private $email;
--- api/src/Form/UserType.php
+++ api/src/Form/UserType.php
@@ -2,12 +2,16 @@
 
 namespace App\Form;
 
+use App\Entity\Structure;
 use App\Entity\User;
+use App\Form\StructureType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\Validator\Constraints\Email;
@@ -17,34 +21,36 @@ class UserType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('firstname', null, ['constraints' => new NotBlank])
-            ->add('lastname', null, ['constraints' => new NotBlank])
-            ->add('email', EmailType::class, ['constraints' => [
-                    new Email,
-                    new NotBlank,
-                ],
-            ])
-            // ->add('email', EmailType::class, ['constraints' => new NotBlank])
-            ->add('password', RepeatedType::class, array(
-                'type' => PasswordType::class,
-                'constraints' => new NotBlank(),
-                'invalid_message' => 'The password fields must match.',
-                'first_options'  => array('label' => 'Password field is required'),
-                'second_options' => array('label' => 'Confirm Password'),
-                )
-            )
-            ->add('biography', null, ['required' => false])
-            // ->add('createdAt')
-            // ->add('updatedAt')
-            // ->add('favorite')
-            ->add('structures', null, ['required' => false])
-        ;
+        ->add('firstname', null, ['constraints' => new NotBlank])
+        ->add('lastname', null, ['constraints' => new NotBlank])
+        ->add('email', EmailType::class, ['constraints' => [
+                new Email,
+                new NotBlank,
+            ],
+        ])
+        ->add('password', PasswordType::class, ['constraints' => new NotBlank])
+            //'type' => PasswordType::class,
+            // 'constraints' => new NotBlank(),
+            //'invalid_message' => 'The password fields must match.',
+            //'first_options'  => array('label' => 'Password field is required'),
+            //'second_options' => array('label' => 'Confirm Password'),
+            //'mapped'=> false,
+        ->add('biography', null, ['required' => false]);
+        // ->add('createdAt')
+        // ->add('updatedAt')
+        // ->add('favorite')
+        /*->add('structures', CollectionType::class, [
+            'entry_type' => StructureType::class,
+            'entry_options' => ['label' => false],
+            'mapped' => false,
+        ])*/
     }
 
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults([
-            'data_class' => User::class,
+            'data_class' => User::class, 
+            'csrf_protection' => false,        
         ]);
     }
 }
