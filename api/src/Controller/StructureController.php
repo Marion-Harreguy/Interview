79188ff5201f3f0d56232e01188683824acb1244
Marion-Harreguy <marionharreguy@gmail.com>
2020-04-17 16:52:32 +0200

    [func] methods Add, Edit for api_structures

--- api/src/Controller/StructureController.php
+++ api/src/Controller/StructureController.php
@@ -4,15 +4,14 @@ namespace App\Controller;
 
 use App\Entity\Structure;
 use App\Form\StructureType;
-use App\Repository\StructureRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Serializer\SerializerInterface;
 
 
     /**
-     * @Route("/api/structures", name="structure-")
+     * @Route("/api/structures", name="structure_")
      */
 class StructureController extends AbstractController
 {
@@ -21,19 +20,14 @@ class StructureController extends AbstractController
      * 
      * @Route("/", name="browse", methods={"GET"})
      */
-    public function browse(StructureRepository $structureRepository, SerializerInterface $serializer)
+    public function browse()
     {
-        $structures = $structureRepository->findAll();
-        $data = $serializer->normalize($structures);
-
-        return $this->json(
-            $data,
-            $status = 200,
-            $headers = ['content-type' => 'application/Json'],
-            $context = []
-        );
+        return $this->json([
+            'message' => 'Welcome on the Browse method',
+            'path' => 'src/Controller/StructureController.php',
+        ]);
     }
-    
+
     /**
      * Affiche une structure spécifique 
      * 
@@ -46,49 +40,55 @@ class StructureController extends AbstractController
             'id' => $id,
         ]);
     }
+
     /**
      * Modifie / met à jour une structure
      * 
      * @Route("/{id}", name="edit", methods={"PUT", "PATCH"}, requirements={"id":"\d+"})
      */
-    public function edit(Structure $structure, Request $request)
+    public function edit(Structure $structure, Request $request, EntityManagerInterface $em)
     {
+        $data = json_decode($request->getContent(), true);
+
         $form = $this->createForm(StructureType::class, $structure);
-        $form->handleRequest($request);
+        $form->submit($data["structure"]);
 
         if($form->isSubmitted() && $form->isValid()) {
 
-            $this->getDoctrine()->getManager()->flush();
+            $em->persist($structure);
+            $em->flush($structure);
 
+            return $this->json(['Structure updated'], $status = 200, $headers = ['content-type' => 'application/Json'], $context = []);
+        } else {
+
+            return $this->json(['Oh, Houston, we have a problem.'], $status = 403, $headers = ['content-type' => 'application/Json'], $context = []);
         }
-
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/StructureController.php',
-        ]);
     }
+
     /**
      * Créer une nouvelle structure
      * 
      * @Route("/", name="add", methods={"POST"})
      */
-    public function add(Request $request)
+    public function add(Request $request, EntityManagerInterface $em)
     {
+        $data = json_decode($request->getContent(), true);
+
         $structure = new Structure();
         $form = $this->createForm(StructureType::class, $structure);
 
-        $form->handleRequest($request);
-
+        $form->submit($data["structure"]);
+        
         if($form->isSubmitted() && $form->isValid()) {
-            $em = $this->getDoctrine()->getManager();
 
             $em->persist($structure);
             $em->flush();
+
+            return $this->json(['Structure added'], $status = 201, $headers = ['content-type' => 'application/Json'], $context = []);
+        } else {
+
+            return $this->json(['Oh, Houston, we have a problem.'], $status = 403, $headers = ['content-type' => 'application/Json'], $context = []);
         }
 
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/StructureController.php',
-        ]);
     }
 }
