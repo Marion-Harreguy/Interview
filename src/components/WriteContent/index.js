21f3e0c4d8b82d4d9a0399faa4597e5a0e93b323
Gael Glotin <gaelglotin@macbook-air-de-gael.home>
2020-04-22 08:59:51 +0200

    update

--- src/components/WriteContent/index.js
+++ src/components/WriteContent/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+ import React from 'react';
 import './style.scss';
 
 const WriteContent = ({ addNewQuestion, addNewAnswer, writeInterview, updateQuestion, updateAnswer, updateContext }) => {
