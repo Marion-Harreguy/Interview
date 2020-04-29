1c035b6ab764ebeab1ea8c901e8643a135557c1d
Arnaud_Oltra <arnaudoltra@hotmail.fr>
2020-04-29 14:12:48 +0200

    [fix] remove interviewed

--- api/src/Controller/InterviewController.php
+++ api/src/Controller/InterviewController.php
@@ -213,32 +213,29 @@ class InterviewController extends AbstractController
     */
             foreach ($interviewedList as $dataInterviewed) {
 
-              
-
-               if(count($interviewedList) < count($interview->getInterviewed())){
-                $interviewedInbase = $interview->getInterviewed();
-                $interviewedNotSaved = $interviewedList;
-
-                    for ($i=0; $i < count($interviewedInbase); $i++) { 
-
-
-                        if (isset($interviewedNotSaved[$i]["email"]) 
-                        && $interviewedInbase[$i]->getEmail() 
-                        === $interviewedNotSaved[$i]["email"]) {
+                if (count($interviewedList) < count($interview->getInterviewed())) {
+                    $interviewedInbase = $interview->getInterviewed();
+                    $interviewedNotSaved = $interviewedList;
+                    for ($i = 0; $i < count($interviewedInbase); $i++) {
+                        if (isset($interviewedNotSaved[$i]["email"]) && $interviewedInbase[$i]->getEmail() === $interviewedNotSaved[$i]["email"]) {
                         } else {
-                            $interview->removeInterviewed($interviewedInbase[$i]);                          
+                            $interview->removeInterviewed($interviewedInbase[$i]);
                         }
-
-
                     }
+                }
 
-
-               }
 
                 if (!empty($dataInterviewed["email"])) {
-                    if ($dataInterviewed["email"] == "anonyme@inter.view") {
-                        $interviewed = $interviewedRepository->findOneBy(["email" => "anonyme@inter.view"]);
-                        $interview->addInterviewed($interviewed);
+
+                    if (count($interviewedList) == count($interview->getInterviewed())) {
+                        if ($dataInterviewed["email"] == "anonyme@inter.view") {
+                            $interviewed = $interviewedRepository->findOneBy(["email" => "anonyme@inter.view"]);
+                            if ($interview->getInterviewed() !=  $interviewed) {
+                                $interview->removeInterviewed($interview->getInterviewed()[0]);
+                                $interview->addInterviewed($interviewed);
+                                //dd($interview->getInterviewed());
+                            }
+                        }
                     }
 
                     $interviewed = $interviewedRepository->findOneBy(["email" => $dataInterviewed["email"]]);
